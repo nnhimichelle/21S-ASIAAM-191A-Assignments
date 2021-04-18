eea107debed2ef0b68b011b0b370e1a3ce8a13fd
nnhimichelle <nnhimichelle@gmail.com>
2021-04-17 21:24:30 -0700

    only focus on week 2

---
+++ Week_03/js/init.js
@@ -0,0 +1,22 @@
+let zoomLevel = 5;
+const mapCenter = [34.0709,-118.444];
+
+let mapConfig = {"center":[34.709,-118.444],"zoomLevel":5}
+
+
+const map = L.map('map').setView(mapCenter, zoomLevel);
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// adding markers
+let work = L.marker([34.0709, -118.444]).addTo(map)
+		.bindPopup('Where I work on campus')
+
+let home = L.marker([37.7409, -122.484]).addTo(map)
+		.bindPopup('Where I currently am')
+
+let random = L.marker([39.7409, -122.484]).addTo(map)
+		.bindPopup('Third Point')
