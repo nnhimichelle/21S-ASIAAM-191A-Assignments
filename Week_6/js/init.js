b4335a14724af266ebd6f08fc7158ea171369220
nnhimichelle <nnhimichelle@gmail.com>
2021-05-12 15:07:23 -0700

    update lab 5

--- Week_6/js/init.js
+++ Week_6/js/init.js
@@ -5,12 +5,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 function addMarker(data){
-        // console.log(data)
-        // these are the names of our lat/long fields in the google sheets:
-        L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${data.timestamp}</h2>`)
-        createButtons(data.lat,data.lng,data.location)
-        return data.timestamp
+    L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${data.location}</h2>${"What makes this place special: " + data.placespecial} <h2>`)
+    createButtons(data.lat,data.lng,data.location);
+    return data   
 }
+
 
 function createButtons(lat,lng,title){
         const newButton = document.createElement("button"); // adds a new button
@@ -19,7 +18,7 @@ function createButtons(lat,lng,title){
         newButton.setAttribute("lat",lat); // sets the latitude 
         newButton.setAttribute("lng",lng); // sets the longitude 
         newButton.addEventListener('click', function(){
-            map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+            map.flyTo([lat,lng], 10); //this is the flyTo from Leaflet
         })
         const spaceForButtons = document.getElementById("contents")
         spaceForButtons.appendChild(newButton);//this adds the button to our page.
@@ -36,7 +35,6 @@ fetch(url)
                 formatData(data)
         }
 )
-
 
 function formatData(theData){
         const formattedData = [] /* this array will eventually be populated with the contents of the spreadsheet's rows */
